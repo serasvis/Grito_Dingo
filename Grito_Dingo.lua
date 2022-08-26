88a3db68ab73a8a7e34f8e158ebd84b88661c04b
serasvis <112015396+serasvis@users.noreply.github.com>
2022-08-26 16:49:02 -0400

    Update Grito_Dingo.lua
    GIF playtime matched to audio file time, and play behavior linked with your FPS (the higher your FPS the faster it will play). Frames are loaded 2 frames ahead of time in an attempt to limit flashing I believe was caused by frames that hadn't fully loaded being hidden. Option to turn audio off added in case your sanity needs to.

--- Grito_Dingo.lua
+++ Grito_Dingo.lua
@@ -8,6 +8,7 @@ frame_scale=0.5
 frame_x=0
 frame_y=350
 syncing=true
+audio=true
 threshold=12
 last_triggered=0
 local frame = CreateFrame( "FRAME" )
@@ -24,19 +25,30 @@ local text2=frame:CreateFontString(nil,"OVERLAY","GameFontNormal")
 text2:SetFont("Interface\\AddOns\\Grito_Dingo\\aAvocadoTaco.ttf", 30)
 text2:SetPoint("CENTER",0,-240)
 text2:SetText("")
-text2:SetTextColor(177/255,127/255,77/255,1)
+text2:SetTextColor(227/255,127/255,27/255,1)
 local mb_index = 0
 local ci_index = 1
-local current_frame = 0
-local total_frames = 624
+local show_frame = 1
+local start_time = 0
+local total_time = 11.2
 local mband1 = frame:CreateTexture()
 local mband2 = frame:CreateTexture()
+local mband3 = frame:CreateTexture()
 mband1:SetAllPoints(frame)
 mband2:SetAllPoints(frame)
+mband3:SetAllPoints(frame)
 local cnftti1 = frame:CreateTexture()
 local cnftti2 = frame:CreateTexture()
+local cnftti3 = frame:CreateTexture()
 cnftti1:SetAllPoints(frame)
 cnftti2:SetAllPoints(frame)
+cnftti3:SetAllPoints(frame)
+mband1:SetTexture("Interface\\AddOns\\Grito_Dingo\\MBAND\\file-"..mb_index..".tga")
+cnftti1:SetTexture("Interface\\AddOns\\Grito_Dingo\\CNFTTI\\file-"..ci_index..".tga")
+if mb_index==47 then mb_index=0 else mb_index=mb_index+1 end
+if ci_index==299 then ci_index=1 else ci_index=ci_index+1 end
+mband2:SetTexture("Interface\\AddOns\\Grito_Dingo\\MBAND\\file-"..mb_index..".tga")
+cnftti2:SetTexture("Interface\\AddOns\\Grito_Dingo\\CNFTTI\\file-"..ci_index..".tga")
 frame:Hide()
 local t_onload = 0
 local t_this_lvl = 0
@@ -46,38 +58,65 @@ local function updateFrame()
 	frame:SetPoint("CENTER",frame_x,frame_y)
 end
 
+local function playSound()
+	if audio then
+		PlaySoundFile("Interface\\Addons\\Grito_Dingo\\grito_mexicano.ogg", "Master")
+	end
+end
+
 local function display_GIF()
-	if current_frame==total_frames then
+	if GetTime()-start_time>=total_time then
 		mb_index=0
 		ci_index=1
-		current_frame=0
+		show_frame=1
+		mband1:SetTexture("Interface\\AddOns\\Grito_Dingo\\MBAND\\file-"..mb_index..".tga")
+		cnftti1:SetTexture("Interface\\AddOns\\Grito_Dingo\\CNFTTI\\file-"..ci_index..".tga")
+		if mb_index==47 then mb_index=0 else mb_index=mb_index+1 end
+		if ci_index==299 then ci_index=1 else ci_index=ci_index+1 end
+		mband2:SetTexture("Interface\\AddOns\\Grito_Dingo\\MBAND\\file-"..mb_index..".tga")
+		cnftti2:SetTexture("Interface\\AddOns\\Grito_Dingo\\CNFTTI\\file-"..ci_index..".tga")
 		frame:Hide()
 	else
-		if mb_index==48 then mb_index=0 end
-		if ci_index==300 then ci_index=1 end
-		if mband1:IsShown() then
-			mband2:SetTexture("Interface\\AddOns\\Grito_Dingo\\MBAND\\file-"..mb_index..".tga")
-			cnftti2:SetTexture("Interface\\AddOns\\Grito_Dingo\\CNFTTI\\file-"..ci_index..".tga")
+		if show_frame==1 then
+			mband1:Show()
+			cnftti1:Show()
+			show_frame=2
+			C_Timer.After(0, function()
+				mband3:Hide()
+				cnftti3:Hide()
+				if mb_index==47 then mb_index=0 else mb_index=mb_index+1 end
+				if ci_index==299 then ci_index=1 else ci_index=ci_index+1 end
+				mband3:SetTexture("Interface\\AddOns\\Grito_Dingo\\MBAND\\file-"..mb_index..".tga")
+				cnftti3:SetTexture("Interface\\AddOns\\Grito_Dingo\\CNFTTI\\file-"..ci_index..".tga")
+				C_Timer.After(0, display_GIF)
+			end)
+		elseif show_frame==2 then
 			mband2:Show()
 			cnftti2:Show()
+			show_frame=3
 			C_Timer.After(0, function()
 				mband1:Hide()
 				cnftti1:Hide()
+				if mb_index==47 then mb_index=0 else mb_index=mb_index+1 end
+				if ci_index==299 then ci_index=1 else ci_index=ci_index+1 end
+				mband1:SetTexture("Interface\\AddOns\\Grito_Dingo\\MBAND\\file-"..mb_index..".tga")
+				cnftti1:SetTexture("Interface\\AddOns\\Grito_Dingo\\CNFTTI\\file-"..ci_index..".tga")
+				C_Timer.After(0, display_GIF)
 			end)
 		else
-			mband1:SetTexture("Interface\\AddOns\\Grito_Dingo\\MBAND\\file-"..mb_index..".tga")
-			cnftti1:SetTexture("Interface\\AddOns\\Grito_Dingo\\CNFTTI\\file-"..ci_index..".tga")
-			mband1:Show()
-			cnftti1:Show()
+			mband3:Show()
+			cnftti3:Show()
+			show_frame=1
 			C_Timer.After(0, function()
 				mband2:Hide()
 				cnftti2:Hide()
+				if mb_index==47 then mb_index=0 else mb_index=mb_index+1 end
+				if ci_index==299 then ci_index=1 else ci_index=ci_index+1 end
+				mband2:SetTexture("Interface\\AddOns\\Grito_Dingo\\MBAND\\file-"..mb_index..".tga")
+				cnftti2:SetTexture("Interface\\AddOns\\Grito_Dingo\\CNFTTI\\file-"..ci_index..".tga")
+				C_Timer.After(0, display_GIF)
 			end)
 		end
-		mb_index=mb_index+1
-		ci_index=ci_index+1
-		current_frame=current_frame+1
-		C_Timer.After(0.014, display_GIF)
 	end
 end
 
@@ -86,32 +125,29 @@ function secondsToDays(inputSeconds)
 	local fhours = math.floor((bit.mod(inputSeconds,86400))/3600)
 	local fminutes = math.floor(bit.mod((bit.mod(inputSeconds,86400)),3600)/60)
 	local fseconds = math.floor(bit.mod(bit.mod((bit.mod(inputSeconds,86400)),3600),60))
-	local tempString = fseconds.."s"
-	local toreturn = tempString
- 	if fminutes > 0 then
-		toreturn = fminutes.."m "..tempString
-		tempString = toreturn
-		if fhours > 0 then
-			toreturn = fhours.."h "..tempString
-			tempString = toreturn
-			if fdays > 0 then
-				toreturn = fdays.."d "..tempString
-			end
-		end
- 	end
- 	return toreturn
+ 	if fdays > 0 then
+		return fdays.."d"..fhours.."h"..fminutes.."m"..fseconds.."s"
+	elseif fhours > 0 then
+		return fhours.."h"..fminutes.."m"..fseconds.."s"
+	else
+ 		return fminutes.."m"..fseconds.."s"
+	end
 end
 
 SLASH_GRITO1 = "/grito";	
 SlashCmdList["GRITO"] = function(msg)
 
 	if msg=="test" then
+		start_time=GetTime()
 		frame:Show()
-		PlaySoundFile("Interface\\Addons\\Grito_Dingo\\grito_mexicano.ogg", "master")
+		playSound()
 		display_GIF()
 		text1:SetText("¡This is a test!")
 		text2:SetText("You've been level "..UnitLevel("player").." for "..secondsToDays(t_this_lvl+(GetTime()-t_onload)) )
 		
+	elseif msg=="test2" then
+		C_ChatInfo.SendAddonMessage("GRITO_BROADCAST", "name=Seravis, level=202, time=2h69m420s", "GUILD")
+
 	elseif string.sub(msg,1,6)=="scale " then
 		local num = tonumber(string.sub(msg,7,-1))
 		if num and type(num)=="number" then
@@ -144,9 +180,19 @@ SlashCmdList["GRITO"] = function(msg)
 
 	elseif msg=="sync on" then
 		syncing=true
+		print("syncing turned on")
 
 	elseif msg=="sync off" then
 		syncing=false
+		print("syncing turned off")
+
+	elseif msg=="audio on" then
+		audio=true
+		print("audio turned on")
+
+	elseif msg=="audio off" then
+		audio=false
+		print("audio turned off")
 
 	elseif string.sub(msg,1,10)=="threshold " then
 		local num = tonumber(string.sub(msg,11,-1))
@@ -162,25 +208,22 @@ SlashCmdList["GRITO"] = function(msg)
 		frame_x=0
 		frame_y=350
 		syncing=true
+		audio=true
 		threshold=12
 		updateFrame()
 		print("default settings restored")
 
 	else
 		print(" /grito Usage:")
-		print("Test Event:")
 		print(" /grito test - trigger test event")
-		print("Frame Scale (Default 0.5):")
-		print(" /grito scale [num] - set frame scale to [num]")
-		print("Frame Position (Default x=0,y=350):")
-		print(" /grito x [num] - change x axis to [num] pixels from center")
-		print(" /grito y [num] - change y axis")
-		print("Syncing (Default ON):")
-		print(" /grito sync on - toggle on amigo broadcasts triggering the event")
+		print(" /grito scale [num] - (default 0.5) set frame scale to [num]")
+		print(" /grito x [num] - (default x=0) change x axis to [num] pixels from center")
+		print(" /grito y [num] - (default y=350) change y axis")
+		print(" /grito sync on - (default on) toggle on amigo broadcasts triggering the event")
 		print(" /grito sync off - toggle it off >:(")
-		print("Threshold (Default 12):")
-		print(" /grito threshold [num] - wait a minimum of [num] seconds before allowing broadcasts to retrigger the event. note: your own level ups are not blocked but do restart this timer")
-		print("Restore Default Settings:")
+		print(" /grito audio on - (default on) toggle on the mariachi music audio clip")
+		print(" /grito audio off - toggle it off >:(")
+		print(" /grito threshold [num] - (default 12) wait a minimum of [num] seconds before allowing broadcasts to retrigger the event. note: your own level ups are not blocked but do restart this timer")
 		print(" /grito default - revert changes to the default settings referenced above")
 	end
 end
@@ -192,20 +235,17 @@ local function EventHandler( self, event, ... )
 
 	if event == "PLAYER_LEVEL_UP" then
 		last_triggered=t
+		start_time=t
 		frame:Show()
-		PlaySoundFile("Interface\\Addons\\Grito_Dingo\\grito_ranchero.ogg", "master")
-		if not current_frame==0 then
-			current_frame=0
-		else
-			display_GIF()
-		end
+		playSound()
+		display_GIF()
 		local t_to_level = secondsToDays(t_this_lvl+(t-t_onload))
 		t_this_lvl=0
 		t_onload=t
 		text1:SetText("¡You've just reached level "..arg1.."!")
 		text2:SetText("You were level ".. arg1-1 .." for "..t_to_level)
-		SendAddonMessage("GRITO_BROADCAST", "name="..UnitName("player")..", level="..arg1..", time="..t_to_level, "GUILD")
 		SendChatMessage("¡I just reached level "..arg1..", amigos!", "GUILD")
+		C_ChatInfo.SendAddonMessage("GRITO_BROADCAST", "name="..UnitName("player")..", level="..arg1..", time="..t_to_level, "GUILD")
 	end
 
 	if event == "CHAT_MSG_ADDON" and arg1=="GRITO_BROADCAST" and syncing and t-last_triggered > threshold then
@@ -213,34 +253,29 @@ local function EventHandler( self, event, ... )
 		for k, v in string.gmatch( arg2, "(%w+)=(%w+)" ) do
 			broadcast[k] = v
 		end
-		last_triggered=t
-		frame:Show()
-		PlaySoundFile("Interface\\Addons\\Grito_Dingo\\grito_ranchero.ogg", "master")
-		display_GIF()
-		text1:SetText("¡"..broadcast.name.." just reached level "..broadcast.level.."!")
-		text2:SetText("They were level ".. tonumber(broadcast.level)-1 .." for "..broadcast.time)
+		if broadcast.name~=UnitName("player") then
+			last_triggered=t
+			start_time=t
+			frame:Show()
+			playSound()
+			display_GIF()
+			text1:SetText("¡"..broadcast.name.." just reached level "..broadcast.level.."!")
+			text2:SetText("They were level ".. tonumber(broadcast.level)-1 .." for "..broadcast.time)
+		end
 	end
 
 	if event == "ADDON_LOADED" and arg1=="Grito_Dingo" then
 		if not frame_scale then
 			frame_scale=0.5
-		end
-		if not frame_x then
 			frame_x=0
-		end
-		if not frame_y then
 			frame_y=350
-		end
-		updateFrame()
-		if not syncing then
 			syncing=true
-		end
-		if not threshold then
+			audio=true
 			threshold=12
-		end
-		if not last_triggered then
 			last_triggered=0
 		end
+		updateFrame()
+		C_ChatInfo.RegisterAddonMessagePrefix("GRITO_BROADCAST")
 		RequestTimePlayed()
 	end
 
@@ -251,7 +286,7 @@ local function EventHandler( self, event, ... )
 end
 
 frame:RegisterEvent( "PLAYER_LEVEL_UP" )
-frame:RegisterEvent( "CHAT_MSG_ADDON" ) --Send addon msg with SendAddonMessage(prefix, msg, type, target)
+frame:RegisterEvent( "CHAT_MSG_ADDON" )
 frame:RegisterEvent( "ADDON_LOADED" )
 frame:RegisterEvent( "TIME_PLAYED_MSG" )
 frame:SetScript( "OnEvent", EventHandler )
